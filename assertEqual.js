785b99d7423718e9a7b90e46a1fb0768b0127862
Ryan Williams <r.d.williams.ca@gmail.com>
2022-02-07 23:30:48 +0000

    assert equal function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅Assertion passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌Assertion failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Hey", "Hey");
+assertEqual(2 + 6, 4 + 4);
+assertEqual(false, true);
+assertEqual(3, 8);
