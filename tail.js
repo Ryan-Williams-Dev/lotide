d3810ea9f387a3aeb1e5bd4df29a0fe632006cb0
Ryan Williams <r.d.williams.ca@gmail.com>
2022-02-08 00:13:41 +0000

    tail function

---
+++ tail.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ - Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ - Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(arr) {
+  let result = arr.slice(1);
+  return result;
+};
+
+const result = tail(['Hello', 'Lighthouse', 'Labs']);
+assertEqual(result.length, 2);
+assertEqual(result[0], 'Lighthouse');
+assertEqual(result[1], 'Labs');
+
+console.log(tail(['Hello']));
+console.log(tail([]));
