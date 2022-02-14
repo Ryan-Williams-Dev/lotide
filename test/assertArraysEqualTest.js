c62862f99ef7783cdfa2905a04be9e38849cfc80
Ryan Williams <r.d.williams.ca@gmail.com>
2022-02-14 23:09:13 +0000

    refactored tail tests to use chai

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,3 +1,11 @@
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
+
+// describe('#head', () => {
+//   it('should return true', () => {
+    
+//   });
+// });
+
 
 assertArraysEqual([1,2,3], [1, 2, 3]);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,16 @@
 const tail = require('../tail');
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-const result = tail(['Hello', 'Lighthouse', 'Labs']);
-assertEqual(result.length, 2);
-assertEqual(result[0], 'Lighthouse');
-assertEqual(result[1], 'Labs');
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+describe('#Tail', () => {
+  it("should return ['Lighthouse', 'Labs'] when given ['Hello', 'Lighthouse', 'Labs']", () => {
+    const actual = tail(['Hello', 'Lighthouse', 'Labs']);
+    const exppected = ['Lighthouse', 'Labs'];
+    assert.deepEqual(actual, exppected);
+  });
+  it('should return [2,3,4,5] when passed [1,2,3,4,5]', () => {
+    const actual = tail([1,2,3,4,5]);
+    const exppected = [2,3,4,5];
+    assert.deepEqual(actual, exppected);
+  });
+});
