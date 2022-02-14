2af26821c664a25e0b734a91013d2186e43902c2
Ryan Williams <r.d.williams.ca@gmail.com>
2022-02-14 21:47:30 +0000

    refactored head and made a test file

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,14 @@
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([3]), 2);
-assertEqual(head([]), "Woo");
+describe('#head', () => {
+  it('returns 5 for [5,6,7]', () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  });
+  it("returns 7 for [7,1,3,4,5,8,7,5]", () => {
+    assert.strictEqual(head([7,1,3,4,5,8,7,5]), 7);
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  })
+});
