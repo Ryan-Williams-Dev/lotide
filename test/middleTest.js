26e99657072a74dafe350115dcd7368a427d9e4a
Ryan Williams <r.d.williams.ca@gmail.com>
2022-02-14 23:18:10 +0000

    refactored middle test

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,27 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1,2,3,4,5,6,7,8,9,10]), [5,6]);
-assertArraysEqual(middle([1,2,3,4,5,6,7]), [4]);
-assertArraysEqual(middle([1,2,3]), [2]);
-assertArraysEqual(middle([1,2]), []);
+
+
+describe('#middle', () => {
+  it ('should return [5,6] when passed [1,2,3,4,5,6,7,8,9,10]', () => {
+    const actual = middle([1,2,3,4,5,6,7,8,9,10]);
+    const expected = [5,6];
+    assert.deepEqual(actual, expected);
+  });
+  it ('should return [4] when passed [1,2,3,4,5,6,7]', () => {
+    const actual = middle([1,2,3,4,5,6,7]);
+    const expected = [4];
+    assert.deepEqual(actual, expected);
+  });
+  it ('should return [] when passed [1,2]', () => {
+    const actual = middle([1,2]);
+    const expected = [];
+    assert.deepEqual(actual, expected);
+  });
+  it ('should return [2] when passed [1,2,3]', () => {
+    const actual = middle([1,2,3]);
+    const expected = [2];
+    assert.deepEqual(actual, expected);
+  });
+});
