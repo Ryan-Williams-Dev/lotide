8e2c9ad9703f6911c60aa4384408e0425e0c30de
Ryan Williams <r.d.williams.ca@gmail.com>
2022-02-14 17:57:22 +0000

    refactored eqArrays for testing

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ - Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ - Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   let matches = true;
   if (arr1.length !== arr2.length) {
@@ -26,17 +18,4 @@ const eqArrays = function(arr1, arr2) {
   return matches;
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
-
-// RECURSION TESTS FOR NESTED ARRAYS
-
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true) // => true
-
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false) // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false) // => false
-
-assertEqual(eqArrays([[[[1,2],2,3,3],4],8], [[[[1,2],2,3,3],4],8]), true);
+module.exports = eqArrays;
---
+++ test/eqArraysTest.js
@@ -0,0 +1,18 @@
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
+
+// RECURSION TESTS FOR NESTED ARRAYS
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true) // => true
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false) // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false) // => false
+
+assertEqual(eqArrays([[[[1,2],2,3,3],4],8], [[[[1,2],2,3,3],4],8]), true);
